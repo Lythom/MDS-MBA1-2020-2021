c756eb36f041329e426200f6da3c3b5b66c56a65
Samuel Bouchet <contact@samuel-bouchet.fr>
2020-10-21 15:37:45 +0200

    Have ball react to physics

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 using UnityEngine;
 
-public class MoveBall : MonoBehaviour
-{
+public class MoveBall : MonoBehaviour {
     public float Speed = 3;
+    public float ShapeRecoverRate = 0.05f;
+
+    private Rigidbody2D rigidbody2D;
 
     private DateTime _nextChangeTime = DateTime.Now;
-    private Vector3 _orientation = Vector3.right;
 
-    void Update()
-    {
-        if (_nextChangeTime < DateTime.Now)
-        {
-            _orientation = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f), 0).normalized;
-            _nextChangeTime = DateTime.Now.AddSeconds(1);
+    private void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        if (_nextChangeTime < DateTime.Now) {
+            GetComponent<SpriteRenderer>().color = Color.white;
         }
+        rigidbody2D.rotation = Mathf.Rad2Deg * Mathf.Atan2(rigidbody2D.velocity.y, rigidbody2D.velocity.x);
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, ShapeRecoverRate);
+    }
 
-        transform.position = transform.position + _orientation * (Speed * Time.deltaTime);
-        transform.localEulerAngles = new Vector3(0, 0, Mathf.Rad2Deg * Mathf.Atan2(_orientation.y, _orientation.x));
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            _nextChangeTime = DateTime.Now.AddMilliseconds(150);
+            transform.localScale = new Vector3(1.1f, 0.7f, 1f);
+        }
     }
 }
