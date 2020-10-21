12ec4be5252230ee565d8f40b366d10c26819998
Samuel Bouchet <contact@samuel-bouchet.fr>
2020-10-21 12:30:12 +0200

    Refactoring

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -5,15 +5,15 @@ public class MoveBall : MonoBehaviour
 {
     public float Speed = 3;
 
-    private DateTime _nextOrientation = DateTime.Now;
+    private DateTime _nextChangeTime = DateTime.Now;
     private Vector3 _orientation = Vector3.right;
 
     void Update()
     {
-        if (_nextOrientation < DateTime.Now)
+        if (_nextChangeTime < DateTime.Now)
         {
             _orientation = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f), 0).normalized;
-            _nextOrientation = DateTime.Now.Add(TimeSpan.FromSeconds(1));
+            _nextChangeTime = DateTime.Now.AddSeconds(1);
         }
 
         transform.position = transform.position + _orientation * (Speed * Time.deltaTime);
